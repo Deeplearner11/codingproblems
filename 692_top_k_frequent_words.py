dbedec0728e9d07113741b5887c2de940211c901
Amitkumar Kumavat <87230145+Deeplearner11@users.noreply.github.com>
2026-05-05 14:31:17 +0530

    Add files via upload

---
+++ 692_top_k_frequent_words.py
@@ -0,0 +1,15 @@
+"""
+692. Top K Frequent Words
+https://leetcode.com/problems/top-k-frequent-words/
+Difficulty: Medium
+Topic: Hash Map, Sorting
+"""
+
+from typing import List
+from collections import Counter
+
+# Time: O(n log n) | Space: O(n)
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = Counter(words)
+        return sorted(freq.keys(), key=lambda x: (-freq[x], x))[:k]
