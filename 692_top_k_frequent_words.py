67fe658c4d0c1f7cb2fc649104c6580420317e79
Amitkumar Kumavat <87230145+Deeplearner11@users.noreply.github.com>
2026-05-06 22:40:55 +0530

    Update 692_top_k_frequent_words.py

--- 692_top_k_frequent_words.py
+++ 692_top_k_frequent_words.py
@@ -13,3 +13,5 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         freq = Counter(words)
         return sorted(freq.keys(), key=lambda x: (-freq[x], x))[:k]
+                                              # First element in tuple (-freq[x]) → Sort by frequency descending
+                                              # Second element (x) → Break ties by alphabetical order
